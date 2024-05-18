054a3c95989f84cc5fd6ccbf3b41c7ce04be7ed7
tanakadays <12brillo.239yuyatnk.a.kappa9ri@gmail.com>
2024-05-18 17:33:25 +0900

    マイグレーションファイルの作成

---
+++ database/migrations/2024_05_16_051524_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->double('latitude')->nullable();#緯度
+            $table->double('longitude')->nullable();#経度
+            $table->string('prace_name', 20);
+            $table->string('genre', 20);
+            $table->string('title_name', 20);
+            $table->string('area', 20);
+            $table->string('information', 500);
+            $table->string('image',100)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
--- database/migrations/2024_05_18_075358_create_category_titles_table.php
+++ database/migrations/2024_05_18_075358_create_category_titles_table.php
@@ -13,14 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('category_titles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->string('image',20)->nullable();
+            $table->string('name', 20);
             $table->timestamps();
         });
     }
@@ -32,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('category_titles');
     }
 };
